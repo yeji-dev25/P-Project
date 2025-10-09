4912f0628f875c2a25b9131e332c4f74faedddc1
yeji-dev25 <jyjjj0329@gachon.ac.kr>
2025-10-10 00:16:23 +0900

    ci: swagger 파일 추가

---
+++ back/src/main/java/com/p_project/home/home.java
@@ -0,0 +1,20 @@
+package com.p_project.home;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class home {
+
+    //  임시 홈
+    @GetMapping("/")
+    public String index() {
+        log.info("Welcom Page 들어옴");
+        return "Welcom";
+    }
+
+}
